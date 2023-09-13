206d99a31ff44c9b28eb415d2bc9a3caf45390a8
Leonardo Deodônio da Silva <leonardodeodonio@gmail.com>
2023-09-12 22:30:30 -0300

    refatorado.

--- index.js
+++ index.js
@@ -45,73 +45,72 @@ allSearch();
 
 function search() {
   const all = document.getElementById("allResult");
-  const busca = document.getElementById("search");
+  const busca = document.getElementById("buscador");
   const retorno = document.getElementById("result");
 
-  console.log(busca);
-  // try {
-  //   axios
-  //     .get(`https://rickandmortyapi.com/api/character/?name=${busca.value}`)
-  //     .then((response) => {
-  //       const result = response.data.results;
-  //       all.style.display = "none";
-  //       console.log("souche");
-  //       console.log(result);
+  try {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/?name=${busca.value}`)
+      .then((response) => {
+        const result = response.data.results;
+        all.style.display = "none";
+        console.log("souche");
+        console.log(result);
 
-  //       for (i in result) {
-  //         const oneCol = document.createElement("col");
-  //         const twoCol = document.createElement("col");
-  //         const treeCol = document.createElement("col");
+        for (i in result) {
+          const oneCol = document.createElement("col");
+          const twoCol = document.createElement("col");
+          const treeCol = document.createElement("col");
 
-  //         const img = document.createElement("img");
+          const img = document.createElement("img");
 
-  //         img.setAttribute("alt", `${result[i].name}`);
-  //         img.setAttribute("class", "imag");
-  //         img.setAttribute("width", "150px");
-  //         img.setAttribute("height", "200px");
-  //         img.setAttribute("src", `${result[i].image}`);
+          img.setAttribute("alt", `${result[i].name}`);
+          img.setAttribute("class", "imag");
+          img.setAttribute("width", "150px");
+          img.setAttribute("height", "200px");
+          img.setAttribute("src", `${result[i].image}`);
 
-  //         const name = document.createElement("p");
-  //         name.innerHTML += `${result[i].name}  ${result[i].id}`;
+          const name = document.createElement("p");
+          name.innerHTML += `${result[i].name}  ${result[i].id}`;
 
-  //         const status = document.createElement("p");
-  //         status.innerHTML += `${result[i].status}`;
+          const status = document.createElement("p");
+          status.innerHTML += `${result[i].status}`;
 
-  //         oneCol.appendChild(img);
-  //         twoCol.appendChild(name);
-  //         treeCol.appendChild(status);
+          oneCol.appendChild(img);
+          twoCol.appendChild(name);
+          treeCol.appendChild(status);
 
-  //         retorno.appendChild(oneCol);
-  //         retorno.appendChild(twoCol);
-  //         retorno.appendChild(treeCol);
+          retorno.appendChild(oneCol);
+          retorno.appendChild(twoCol);
+          retorno.appendChild(treeCol);
 
-  //         // const div = document.createElement("div");
+          // const div = document.createElement("div");
 
-  //         // const element = document.createElement("img");
-  //         // element.setAttribute("alt", `${result[i].name}`);
-  //         // element.setAttribute("class", "imag");
-  //         // element.setAttribute("width", "150px");
-  //         // element.setAttribute("height", "200px");
-  //         // element.setAttribute("src", `${result[i].image}`);
+          // const element = document.createElement("img");
+          // element.setAttribute("alt", `${result[i].name}`);
+          // element.setAttribute("class", "imag");
+          // element.setAttribute("width", "150px");
+          // element.setAttribute("height", "200px");
+          // element.setAttribute("src", `${result[i].image}`);
 
-  //         // const name = document.createElement("p");
-  //         // name.innerHTML += `${result[i].name}  ${result[i].id}`;
+          // const name = document.createElement("p");
+          // name.innerHTML += `${result[i].name}  ${result[i].id}`;
 
-  //         // const status = document.createElement("p");
-  //         // status.innerHTML += `${result[i].status}`;
+          // const status = document.createElement("p");
+          // status.innerHTML += `${result[i].status}`;
 
-  //         // div.appendChild(element);
-  //         // div.appendChild(name);
-  //         // div.appendChild(status);
+          // div.appendChild(element);
+          // div.appendChild(name);
+          // div.appendChild(status);
 
-  //         // retorno.appendChild(div);
-  //       }
-  //     });
-  // } catch (e) {
-  //   const element = document.createElement("p");
-  //   element.innerText = `Dados não encontrados ${e}`;
-  //   retorno.appendChild(element);
-  // }
+          // retorno.appendChild(div);
+        }
+      });
+  } catch (e) {
+    const element = document.createElement("p");
+    element.innerText = `Dados não encontrados ${e}`;
+    retorno.appendChild(element);
+  }
 }
 
 // a desenvolver procurar  clicando no nome da foro
